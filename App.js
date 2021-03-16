e9570662ee580074d8adf992ddb29415cdb8c1d2
AmmanMeb <72285018+AmmanMeb@users.noreply.github.com>
2021-03-16 17:18:54 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Header from './components/headerComponent/header';
+import './App.css';
+
+function App() {
+  return(
+    <Header />
+  )
+}
+export default App;
