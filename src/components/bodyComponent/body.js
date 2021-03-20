73e4023983f58f420df6bbb78fd4c5ef7b8e8c0d
AmmanMeb <72285018+AmmanMeb@users.noreply.github.com>
2021-03-20 18:56:07 -0400

    Update body.js

--- src/components/bodyComponent/body.js
+++ src/components/bodyComponent/body.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import './body.css'
 
-function Body() {  
-  
+class Body extends Component {  
+  constructor(props) {
+    super(props);
     this.state = {
       input: "",
     };
-  
-  const onSubmit = (event) => {
+  }
+  onSubmit = (event) => {
     event.preventDefault()
      if (this.state.input.length != 1) {
        return document.getElementById('error').innerHTML="No email found"
      }
   }
+  render() {
     return(
       <section className = "image-banner">                   
           <div className = "text-1">
@@ -25,11 +27,12 @@ function Body() {
               Type your Email below to recieve further updates
           </div>
           <input>{this.state.input}</input>
-          <button onClick={onSubmit}>Send</button>
+          <button onClick= {this.onSubmit}>Send</button>
           <p id="error"></p>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"></link>
       </section>
       
     )
+  }
   }
   export default Body;
