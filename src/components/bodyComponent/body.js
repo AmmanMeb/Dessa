f8094903c3d0d0b921a48219a0df21e8ed528f88
AmmanMeb <72285018+AmmanMeb@users.noreply.github.com>
2021-03-22 15:33:03 -0400

    Update body.js

--- src/components/bodyComponent/body.js
+++ src/components/bodyComponent/body.js
@@ -16,7 +16,7 @@ class Body extends Component {
        return document.getElementById('error').innerHTML="Please type an email address in the field"
      }
 
-     var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+     var mailformat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
      if(this.state.input.value !== (mailformat))
      {     
       return true;
