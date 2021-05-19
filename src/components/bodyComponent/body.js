7a6c8a7179becf64bf3bfe1b5b15b17358a8abc9
AmmanMeb <72285018+AmmanMeb@users.noreply.github.com>
2021-05-19 17:12:26 -0400

    Add files via upload

--- src/components/bodyComponent/body.js
+++ src/components/bodyComponent/body.js
@@ -29,7 +29,8 @@ class Body extends Component {
  
   render() {
     return(
-      <section className = "image-banner">                   
+      <section className = "image-banner"> 
+        <div className = "index-banner">                  
           <div className = "text-1"> 
               COMING SOON
           </div>
@@ -42,7 +43,8 @@ class Body extends Component {
           <input value = {this.state.input} onChange={(e) => this.setState({ input: e.target.value })} />
           <button onClick= {this.onSubmit}>Send</button>
           <p id="error"></p>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"></link>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"></link>
+        </div>
       </section>
       
     )
