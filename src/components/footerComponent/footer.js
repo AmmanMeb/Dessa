4752e7c89e255debc07be0bb77460622e4427ae3
AmmanMeb <72285018+AmmanMeb@users.noreply.github.com>
2022-01-26 13:16:07 -0500

    Add files via upload

--- src/components/footerComponent/footer.js
+++ src/components/footerComponent/footer.js
@@ -7,36 +7,36 @@ class Footer extends Component {
       return(
         <div className = "index-banner-2">
           <div className='footer-links'>
-            <div className='headings'>
-             <div className='text-4'>SHOP</div>
-             <div className='text-5'>SUPPORT</div>
-             <div className='text-6'>COMPANY</div>
-            </div>
             <div className='footer-links-1'>
-             <div className='text-6'>Mobile</div>
-             <div className='text-7'>Contact Us</div>
-             <div className='text-8'>Careers</div>
+             <div className='headings'>SHOP</div> 
+             <div className='links'>
+              <div className='text-6'>Mobile</div>
+              <div className='text-6'>Tablets</div>
+              <div className='text-6'>Watches</div>
+              <div className='text-6'>Laptops</div>
+              <div className='text-6'>TV's</div> 
+             </div>
             </div>
             <div className='footer-links-2'>
-             <div className='text-9'>Tablets</div>
-             <div className='text-10'>Order Support</div>
-             <div className='text-11'>Investors</div>
+             <div className='headings'>SUPPORT</div> 
+             <div className='links'>
+              <div className='text-6'>Contact Us</div>
+              <div className='text-6'>Order Support</div>
+              <div className='text-6'>Your Account</div>
+              <div className='text-6'>Product Support</div>
+              <div className='text-9'>Accessibility</div> 
+             </div>            
             </div>
             <div className='footer-links-3'>
-             <div className='text-12'>Watches</div>
-             <div className='text-13'>Your Account</div>
-             <div className='text-14'>Newsroom</div>
-            </div>
-            <div className='footer-links-4'>
-             <div className='text-15'>Laptops</div>
-             <div className='text-16'>Product Support</div>
-             <div className='text-17'>Explore</div>
-            </div>
-            <div className='footer-links-5'>
-             <div className='text-18'>TV's</div>
-             <div className='text-19'>Accessibility</div>
-             <div className='text-20'>About Us</div>
-            </div>
+             <div className='headings'>COMPANY</div>
+             <div className='links'>
+              <div className='text-6'>Careers</div>
+              <div className='text-6'>Investors</div>
+              <div className='text-6'>Newsroom</div>
+              <div className='text-6'>Explore</div>
+              <div className='text-6'>About Us</div>
+             </div>
+            </div> 
           </div>
           <div className='text-21'>Copyright© 2021 DESSA All Rights Reserved.</div>
           <div className='footer-links-6'>
